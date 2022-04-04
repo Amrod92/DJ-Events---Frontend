28a854028b781ca6edd53ba791690ad80fd3e93d
Amrod92 <manlio92--@live.it>
2022-04-04 14:30:07 +0100

    Module Aliases

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,8 +1,10 @@
+import Layout from '@/components/Layout';
+
 export default function EventPage() {
   return (
-    <div>
+    <Layout>
       <h1>My event</h1>
       <h3>Enjoy!!</h3>
-    </div>
+    </Layout>
   );
 }
